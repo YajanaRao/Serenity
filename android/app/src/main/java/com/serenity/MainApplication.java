a7c8914587d39dd6aea3aeb492fd1c6e2707fe98
Yajana Rao <yajana.merahkee@gmail.com>
2019-07-13 16:55:38 +0530

    Working release code

--- android/app/src/main/java/com/serenity/MainApplication.java
+++ android/app/src/main/java/com/serenity/MainApplication.java
@@ -3,8 +3,7 @@ package com.serenity;
 import android.app.Application;
 
 import com.facebook.react.PackageList;
-import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
+import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
