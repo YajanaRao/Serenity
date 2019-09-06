cddaecde2e339feee0be52193c41ffd2d5c19162
YajanaRao <yajananrao@gmail.com>
2019-09-06 16:08:33 +0530

    Added app center icon to readme

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import RootNavigator from './RootNavigator';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { ActivityIndicator, View } from 'react-native';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import {ActivityIndicator, View} from 'react-native';
 
 import configureStore from './store';
 
-const { store, persistor } = configureStore();
+const {store, persistor} = configureStore();
 
 class App extends React.Component {
-
   renderActivityIndicator = () => {
-    return(
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <ActivityIndicator size="large"/>
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <ActivityIndicator size="large" />
       </View>
-    )
-  }
+    );
+  };
   render() {
     return (
       <Provider store={store}>
-        <PersistGate loading={this.renderActivityIndicator()} persistor={persistor}>
+        <PersistGate
+          loading={this.renderActivityIndicator()}
+          persistor={persistor}>
           <RootNavigator />
         </PersistGate>
       </Provider>
