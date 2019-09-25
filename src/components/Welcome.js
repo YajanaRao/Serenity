cc7d4b08ab74f5ed4950d2f8eb747d6091a41923
YajanaRao <yajananrao@gmail.com>
2019-09-25 19:06:22 +0530

    Worked on font family

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,8 +5,8 @@ import { ActivityIndicator, Headline, withTheme } from 'react-native-paper';
 const Welcome = (props) => {
     const { colors } = props.theme; 
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: colors.background }}>
-            <Headline style={{ margin: 16, fontSize: 40, fontWeight: 'bold', padding: 16 }} >Serenity</Headline>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            {/* <Headline style={{ margin: 16, fontSize: 48, fontWeight: 'bold', padding: 16 }} >Serenity</Headline> */}
             <ActivityIndicator animating={true} size="large" />
         </View>
     )
