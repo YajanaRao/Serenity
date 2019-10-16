2f23a39a5ec40c87ed0019ce35bc5fa8aa4b7839
Nélson Costa <nelson.fn.costa@gmail.com>
2019-10-16 11:40:49 +0100

    Add lint script

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import RootNavigator from './RootNavigator';
-import {Provider} from 'react-redux';
-import {PersistGate} from 'redux-persist/integration/react';
 
 import configureStore from './store';
 import Welcome from './components/Welcome';
 
-const {store, persistor} = configureStore();
+const { store, persistor } = configureStore();
 
 class App extends React.Component {
-  renderActivityIndicator = () => {
-    return (
-      <Welcome/>
-    );
-  };
+  renderActivityIndicator = () => <Welcome />;
+
   render() {
     return (
       <Provider store={store}>
         <PersistGate
           loading={this.renderActivityIndicator()}
-          persistor={persistor}>
+          persistor={persistor}
+        >
           <RootNavigator />
         </PersistGate>
       </Provider>
