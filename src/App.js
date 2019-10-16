13a9cf3fbbcd450a48f2511ce3e4f7d98a4944bc
YajanaRao <yajananrao@gmail.com>
2019-10-16 20:39:26 +0530

    Multiple import fix🤾‍♂️

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import RootNavigator from './RootNavigator';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 
+import RootNavigator from './RootNavigator';
 import configureStore from './store';
 import Welcome from './components/Welcome';
 
