39746b6e8eb19452ff5c248018ca98c95658e6d2
YajanaRao <yajananrao@gmail.com>
2019-10-24 11:27:49 +0530

    container text issue fix

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import {ImageBackground, View} from 'react-native';
-import {Title} from 'react-native-paper';
+import { ImageBackground, View } from 'react-native';
+import { Title } from 'react-native-paper';
 import PropTypes from 'prop-types';
 
 export default Quote = props => {
   return (
-    <View style={{flex: 1}}>
+    <View style={{ flex: 1 }}>
       <ImageBackground
-        source={{uri: props.backgroundImage}}
-        style={{padding: 20, height: 200}}
+        source={{ uri: props.backgroundImage }}
+        style={{ padding: 20, minHeight: 200 }}
         blurRadius={1}
         // imageStyle={{borderRadius: 4}}
       >
-        <Title style={{fontFamily: 'Feather'}}>{props.quote}</Title>
+        <Title style={{ fontFamily: 'Feather' }}>{props.quote}</Title>
       </ImageBackground>
     </View>
   );
@@ -20,5 +20,5 @@ export default Quote = props => {
 
 Quote.propTypes = {
   backgroundImage: PropTypes.string,
-  quote: PropTypes.string
-}
+  quote: PropTypes.string,
+};
