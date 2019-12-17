600da556b32bf1c88366f3fcfaacb5fa269b86c3
YajanaRao <yajananrao@gmail.com>
2019-12-17 16:46:52 +0530

    new way of styles 👓

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ViewStyle } from 'react-native';
 
 // @ts-ignore
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
+const CONTAINER: ViewStyle = {
+  alignItems: 'center',
+  backgroundColor: '#00F260',
+  flex: 1,
+  justifyContent: 'center',
+};
+
 export const Welcome = () => (
-  <View
-    style={{
-      alignItems: 'center',
-      backgroundColor: '#00F260',
-      flex: 1,
-      justifyContent: 'center',
-    }}
-  >
+  <View style={CONTAINER}>
     <Icon name="music" color="#ffffff" size={40} />
   </View>
 );
