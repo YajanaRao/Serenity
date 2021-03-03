8e0c84265d52b0e4e66eb7869fdd1bb00c65e793
YajanaRao <yajananrao@gmail.com>
2021-03-03 23:15:22 +0530

    Removed app center libraries

--- src/utils/logging.ts
+++ src/utils/logging.ts
@@ -1,12 +1,10 @@
 /* global __DEV__ */
-import Analytics from 'appcenter-analytics';
 import crashlytics from '@react-native-firebase/crashlytics';
 
 export const log = (message: any) => {
   if (__DEV__) {
     console.log(message);
   } else {
-    Analytics.trackEvent('error', message);
     crashlytics().log(message);
     crashlytics().recordError(message);
   }
@@ -16,6 +14,9 @@ export const logEvent = (event: string, message: any) => {
   if (__DEV__) {
     console.log(event, message);
   } else {
-    Analytics.trackEvent(event, message);
+    crashlytics().setAttributes({
+      event,
+      message,
+    });
   }
 };
