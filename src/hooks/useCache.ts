a41ecc90a09854120c6fc59288fc191e276ad9da
YajanaRao <yajananrao@gmail.com>
2021-03-07 12:42:35 +0530

    Handled session management in cache

--- src/hooks/useCache.ts
+++ src/hooks/useCache.ts
@@ -1,19 +1,43 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useEffect, useState } from 'react';
+import { log } from '../utils/logging';
 
 export function useCache(key: string, query: () => Promise<any>) {
   const [data, setData] = useState([]);
 
+  async function queryData() {
+    const response = await query();
+    const sessionData = {
+      data: response,
+      timestamp: new Date(),
+    };
+    AsyncStorage.setItem(key, JSON.stringify(sessionData));
+    return response;
+  }
+
   useEffect(() => {
     const fetchApi = async () => {
       const cache = await AsyncStorage.getItem(key);
+      let response = [];
       if (!cache) {
-        const response = await query();
-        console.log('response', response);
-        AsyncStorage.setItem(key, JSON.stringify(response));
-        setData(response);
+        response = await queryData();
+      } else {
+        const sessionData = JSON.parse(cache);
+        const now = new Date();
+        if (
+          new Date(sessionData.timestamp).toDateString() !== now.toDateString()
+        ) {
+          log(
+            `local cache time ${new Date(
+              sessionData.timestamp,
+            ).toDateString()} and current time ${now.toDateString()}`,
+          );
+          response = await queryData();
+        } else {
+          response = sessionData.data;
+        }
       }
-      setData(JSON.parse(cache));
+      setData(response);
     };
     fetchApi();
   }, []);
