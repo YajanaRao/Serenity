cce419fee2663c47ccf2e6c1da2857e8977eef44
YajanaRao <yajananrao@gmail.com>
2022-04-20 22:49:28 +0530

    Updated packages

--- app/web/src/Home/Home.tsx
+++ app/web/src/Home/Home.tsx
@@ -3,13 +3,11 @@ import { Container } from "@serenity/components";
 import PlaylistList from "./components/PlaylistList";
 
 export function Home() {
- 
 
   return (
-    
-        <Container style={{ flex: 4, overflow: "hidden" }}>
-          <PlaylistList />
-        </Container>
-     
+    <Container style={{ flex: 4, overflow: "hidden" }}>
+      <PlaylistList />
+    </Container>
+
   );
 }
