c8140e98422d3e69ee4d67202933811ea9816886
YajanaRao <yajananrao@gmail.com>
2022-09-21 23:04:52 +0530

    Updated setup script

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -1,3 +1,12 @@
 @echo off
 git submodule update --init
 git submodule update --recursive --remote
+
+@REM install packages
+yarn install
+
+@REM make gradlew executable
+chmod +x app/mobile/android/gradlew
+
+@REM create android release
+yarn release
