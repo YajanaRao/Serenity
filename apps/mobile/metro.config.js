628858eb94c43ac0009119d2d50e6b17b5d2327f
YajanaRao <yajananrao@gmail.com>
2024-07-06 12:06:56 +0530

    chore: Update metro.config.js and package.json for React Native upgrade

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -4,27 +4,31 @@
  *
  * @format
  */
-// eslint-disable-next-line import/no-extraneous-dependencies
-const {getDefaultConfig} = require('metro-config');
+
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 const path = require('path');
 
-module.exports = (async () => {
-	const {
-		resolver: {sourceExts, assetExts},
-	} = await getDefaultConfig();
-	return {
-		watchFolders: [path.resolve(__dirname, '../../')],
-		transformer: {
-			getTransformOptions: () => ({
-				transform: {
-					experimentalImportSupport: false,
-					inlineRequires: true,
-				},
-			}),
-		},
-		resolver: {
-			assetExts: assetExts.filter(ext => ext !== 'svg'),
-			sourceExts: [...sourceExts, 'svg'],
-		},
-	};
-})();
+const defaultConfig = getDefaultConfig(__dirname);
+
+const {
+	resolver: {sourceExts, assetExts},
+} = getDefaultConfig(__dirname);
+
+const config = {
+	watchFolders: [path.resolve(__dirname, '../../')],
+	transformer: {
+		getTransformOptions: async () => ({
+			transform: {
+				experimentalImportSupport: false,
+				inlineRequires: true,
+			},
+		}),
+		babelTransformerPath: require.resolve('react-native-svg-transformer'),
+	},
+	resolver: {
+		assetExts: assetExts.filter(ext => ext !== 'svg'),
+		sourceExts: [...sourceExts, 'svg'],
+	},
+};
+
+module.exports = mergeConfig(defaultConfig, config);
